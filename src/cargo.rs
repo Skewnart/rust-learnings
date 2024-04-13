a5e3d300ae8fdbc2d3eaea4fa34fb54ad0eec8a0
Skewnart <corentinz@outlook.fr>
2024-04-14 00:26:55 +0200

    🧬 adding variables / types / fonctions

--- src/cargo.rs
+++ src/cargo.rs
@@ -6,11 +6,17 @@ cargo new
 
 cargo build ("cargo b")
     --release
-    
+
 cargo run ("cargo r")
 cargo test ("cargo t")
 cargo clean
 
 cargo --version
+
+
+Configuration :
+- On peut configurer le linter rust dans VSC : paramètre "Rust-analyzer › Check: Command"
+    "test" pour le linter classique (valeur par défaut)
+    "clippy" pour le linter amélioré (vraiment pas mal)
 
 */
