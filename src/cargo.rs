214c3c956aee0032beb834d0351115a9acc3fc7a
Skewnart <corentinz@outlook.fr>
2024-04-13 23:34:30 +0200

    🛳️adding cargo file

---
+++ src/cargo.rs
@@ -0,0 +1,16 @@
+/*
+
+cargo new
+    --bin : binary
+    --lib : library
+
+cargo build ("cargo b")
+    --release
+    
+cargo run ("cargo r")
+cargo test ("cargo t")
+cargo clean
+
+cargo --version
+
+*/
