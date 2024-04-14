aa8e694f26d1a207b15b9266a3b9609068da188f
Skewnart <corentinz@outlook.fr>
2024-04-14 23:30:52 +0200

    🛠️ adding test

---
+++ src/test.rs
@@ -0,0 +1,9 @@
+//#[test] pour tranformer une méthode en test
+//assert!()
+//assert_eq!()
+//assert_ne!()
+#[test]
+fn test_example() {
+    let variable: i32 = 10;
+    assert_eq!(variable, 10);
+}
