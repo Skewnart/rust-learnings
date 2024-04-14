51ad644c78f758715ca82c5a3a2ae361670f43f5
Skewnart <corentinz@outlook.fr>
2024-04-14 23:32:29 +0200

    📚adding module part

---
+++ src/module/functions.rs
@@ -0,0 +1,3 @@
+pub fn function(){
+    println!("fonction de module appelable.");
+}
---
+++ src/module/mod.rs
@@ -0,0 +1,3 @@
+//Le présent fichier "mod.rs" sera le module lu pour le module "module"
+//Il permettra de proposer "functions" à l'extérieur grâce à l'instruction suivante
+pub(crate) mod functions;
