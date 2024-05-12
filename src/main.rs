801eaf86fa6a7aee5dd7611377e12041ef1476f2
Skewnart <corentinz@outlook.fr>
2024-05-12 23:43:57 +0200

    ↗ adding types

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     //no exec                               //1. Cargo command with parameters
     //no exec                               //2. Tests (how to write)
 
-    //types::init_var();                    //3. Types
+    types::init_var();                    //3. Types
     //fonctions_variables::call();          //4. Write functions and variables
     //module::functions::function();        //5. Module management
 }
--- src/types.rs
+++ src/types.rs
@@ -6,8 +6,20 @@
 //i8, i16, i32, i64     integers
 //u8, u16, u32, u64     unsigned integers
 //f32, f64              float / double
+//isize, usize          size of target
+//bool                  boolean
+//char                  character
 
 pub fn init_var() {
-    let var_i8 : i8 = 0;
-    assert_eq!(var_i8, 0);
+    let var_i32 : i32 = 0;
+    let var_f64: f64 = 0.1;
+    let var_isize : isize = 64;
+    let var_bool : bool = false;
+    let var_char: char = '\0';
+    
+    print_type_of(&var_isize);
+}
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>());
 }
