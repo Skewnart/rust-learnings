98f76ea28c7796a786fc84129d3fec9ab2e3ae02
Skewnart <corentinz@outlook.fr>
2024-12-30 23:26:39 +0100

    feat: updating functions for consts

--- src/fonctions.rs
+++ src/fonctions.rs
@@ -4,6 +4,11 @@
 //Un paramètre de fonction sera par défaut constant
 // dernière instruction pas besoin de "return" (mais on peut si besoin)
 //pub : public. Une fonction appelable à l'extérieur du module
+//const est stocké dans la mémoire du programme. Il doit être connu au moment de la compilation
+//shadowing : une variable est re-définissable, avec le même nom. Si le shadow est fait dans un scope enfant,
+//   l'ancienne variable sera de nouveau disponible à la fin de celui-ci.
+
+const ONE_HOUR_IN_SECOND : i32 = 3600;
 
 pub fn call(){
     let p = perimetre_cercle(5.0);
@@ -11,6 +16,9 @@ pub fn call(){
 
     println!("Le périmètre du cercle est de {}cm.", p);    
     println!("Le volume de la sphère est de {}cm.", volume_sphere(5.0));
+
+    // shadowing
+    let p = 5;
 }
 
 fn perimetre_cercle(rayon : f64) -> f64 {
