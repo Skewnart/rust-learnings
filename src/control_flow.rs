344775f040cdad599441ff285eec8f450b24927c
Skewnart <corentinz@outlook.fr>
2025-01-16 16:58:20 +0100

    feat: adding Control Flows

---
+++ src/control_flow.rs
@@ -0,0 +1,71 @@
+/*
+    - if, else if, else
+    - if est une expression, il peut assigner une valeur
+    - loop (+ expression + loop labels)
+    - while
+    - for
+*/
+
+pub fn using_controlflows() {
+    let number = 10;
+
+    if number < 5 {
+        println!("Condition true");
+    } else if number == 5 {
+        println!("Other condition true (number equals 5)");
+    } else {
+        println!("Condition false");
+    }
+
+    //if est une expression
+    let number = if number > 10 { 15 } else { 5 };
+
+    //Boucle à break
+    loop {
+        for i in 1..=10 {
+            println!("Number : {number}");
+        }
+        break;
+    }
+
+    //Loop peut renvoyer une valeur
+    let mut counter = 0;
+    let number = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter;
+        }
+    }
+
+    //Loop labels
+    'looplabel : loop {
+        loop {
+            break 'looplabel;
+        }
+    }
+
+    //While
+    let mut number = 3;
+    while number != 0 {
+        println!("{number}!");
+        number -= 1;
+    }
+    
+    //For
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    // If let destructuring
+    let opt = Option::Some(5);
+    if let Some(value) = opt {
+        println!("The value is: {value}");
+    }
+
+    let opt = Option::None;
+    if let Some(value) = opt {
+        println!("The value is: {value}");
+    }
+}
