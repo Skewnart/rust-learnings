8ed1b99de9e1af774564a47cd99f9077b1bc2937
Skewnart <corentinz@outlook.fr>
2025-01-16 16:59:07 +0100

    feat: adding strings

--- src/strings.rs
+++ src/strings.rs
@@ -27,4 +27,23 @@ pub fn using_str() {
     for s in str_3.split("h") {
         println!("{}", s);
     }
+
+    let mut s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+
+    //s1.push_str(&s2);     , ou
+    s1 += &s2;              //Le add du String prend en paramètre une référence, ce qui permet de réutiliser s2 après
+    println!("{}", s1);
+
+    s1 += " - " + &s2;      //Aucune copie particulière n'est faite, il n'y a que des références
+
+    let new_s = format!("{} - {}", s1, s2);     //Même utilisation qu'un println!, sauf que format! n'envoie rien dans la sortie standard, juste il renvoie un string
+
+    //Un caractère Unicode peut être représenté par plusieurs bytes
+    for b in "Зд".bytes() {
+        println!("{b}");
+    }
+    // 208, 151, 208, 180
+
+    
 }
