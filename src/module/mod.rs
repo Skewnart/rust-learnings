6d8fb0a830e11a781391b688dba99dc6a4a54798
Skewnart <corentinz@outlook.fr>
2025-01-16 16:59:18 +0100

    feat: adding mods

--- src/module/mod.rs
+++ src/module/mod.rs
@@ -1,3 +1,17 @@
 //Le présent fichier "mod.rs" sera le module lu pour le module "module"
 //Il permettra de proposer "functions" à l'extérieur grâce à l'instruction suivante
 pub(crate) mod functions;
+
+//"pub" permet d'exposer les différentes fonctions et modules à l'intérieur
+//Pour utiliser une fonction ou module ne nous appartenant pas, il faut utiliser le mot clé "use", potentiellement avec un alias ("as")
+//Pour utiliser une fonction d'un module parent, utiliser "super::nom_de_la_fonction" ou "super::nom_du_module::nom_de_la_fonction"
+
+
+//use std::io;
+//use std::io::Write;
+
+// =
+
+//use std::io::{self, Write};
+
+//use std::collections::*;
